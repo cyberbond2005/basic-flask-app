b9894d467d9b0e1e04d0eea59bf0a2fc0b57fe1a
cyberbond2005 <cyberbond2005@gmail.com>
2021-05-02 19:35:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def main():
     return "Welcome!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
